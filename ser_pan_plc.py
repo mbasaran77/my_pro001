5235b5f8d0b428c1a21c963187a1b883fd870403
mbasaran77 <mbasaran77@gmail.com>
2016-05-06 06:43:49 +0300

    serial pan_plc.py eklendi

--- ser_pan_plc.py
+++ ser_pan_plc.py
@@ -1,3 +1,4 @@
+
 import serial
 
 
@@ -12,7 +13,7 @@ def ser_init():
     seri_port.is_open
 
 mydict={0: ['renk1', 0, 50], 1: ['renk2', 50, 80], 2: ['renk3', 80, 120], 3: ['renk4', 120, 180], 4: ['renk5', 180, 220], 5: ['renk6', 220, 350],
-        6: ['renk1', 500, 900]}
+        6: ['renk1', 500, 900],7: ['renk2', 90, 110], 8: ['renk3', 134, 560], 9: ['renk4', 455, 567], 10: ['renk5', 230, 420], 11: ['renk6', 304, 405]}
 
 def hazirla(mydict):
     x=len(mydict)
@@ -29,34 +30,87 @@ def hazirla(mydict):
     for i in range(x):
         a=mydict[i]
         if 'renk1' in a:
-            renk1.append(a[1])
-            renk1.append(a[2])
+            renk1.append(str(a[1]))
+            renk1.append(str(a[2]))
 
-            mystr=str(a[1])
-            print(mystr)
-
-            b=bytes(mystr,'utf-8')
-            print("renk1",renk1)
-
-            #del a
         elif 'renk2' in a:
-            renk2.append(a[1])
-            renk2.append(a[2])
-            print("renk2", renk2)
+            renk2.append(str(a[1]))
+            renk2.append(str(a[2]))
+        elif 'renk3' in a:
+            renk3.append(str(a[1]))
+            renk3.append(str(a[2]))
+        elif 'renk4' in a:
+            renk4.append(str(a[1]))
+            renk4.append(str(a[2]))
+        elif 'renk5' in a:
+            renk5.append(str(a[1]))
+            renk5.append(str(a[2]))
+        elif 'renk6' in a:
+            renk6.append(str(a[1]))
+            renk6.append(str(a[2]))
+        elif 'renk7' in a:
+            renk7.append(str(a[1]))
+            renk7.append(str(a[2]))
+        elif 'renk8' in a:
+            renk8.append(str(a[1]))
+            renk8.append(str(a[2]))
+        del a
+    print("renk1", renk1)
+    print("renk2", renk2)
+    print("renk3", renk3)
+    print("renk4", renk4)
+    print("renk5", renk5)
+    print("renk6", renk6)
+    print("renk7", renk7)
+    print("renk8", renk8)
+
+oku_str="%01#RDD0010000107**\r"
+oku_str_1="%01#RDD0020000002"
+oku_str_2="%01#RDD0010000107"
+
+
+def bcc_calc(d_str):
+    e = 0
+    _d_str=d_str.encode()
+    for i in _d_str:
+        d=i
+        e=d^e
+    print(chr(e))
+
+    return ("%01#RDD0020000002"+str(e)+"\r").encode()
+
+print(bcc_calc(oku_str_1))
+print(bcc_calc(oku_str_2))
+
+
+
+
+
+
+hazirla(mydict)
+# my_decoded_str = str.decode(bytes)
+# type(my_decoded_str) # insures its string
+
+
+
+
 
 #hazirla(mydict)
-dizi=[1,10,400]
-a=str(dizi[1])
-d=str(dizi[2])
-b=repr(dizi[1])
-print(type(a))
-print(type(b))
-c=bytes(a,'utf-8')+bytes(d,'utf-8')
-print(c)
+# dizi=[1,10,400]
+# a=str(dizi[1])
+# d=str(dizi[2])
+# b=repr(dizi[1])
+# print(type(a))
+# print(type(b))
+# c=bytes(a,'utf-8')+bytes(d,'utf-8')
+# print(c)
 
-mystring ="@00RD0000010057*"
 
-b = bytes(mystring, 'utf-8')
 
-print(b)
+
+# mystring ="@00RD0000010057*"
+#
+# b = bytes(mystring, 'utf-8')
+#
+# print(b)
 
