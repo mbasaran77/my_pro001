a7d593bb79b54179f8c24cbc094adedbbf5e2098
mbasaran77 <mbasaran77@gmail.com>
2016-09-16 17:48:52 +0300

    Merge branch 'master' of https://github.com/mbasaran77/my_pro001
    Conflicts:
    	vpRecete.ui

---
+++ modbus_d.py
@@ -0,0 +1,42 @@
+
+from pyModbusTCP.client import ModbusClient
+from pyModbusTCP import utils
+import time
+
+SERVER_HOST = "192.168.250.2"
+SERVER_PORT = 502
+
+c = ModbusClient()
+
+c.host(SERVER_HOST)
+c.port(SERVER_PORT)
+
+if not c.is_open():
+    if not c.open():
+        print("unable to connect to " + SERVER_HOST + ":" + str(SERVER_PORT))
+
+
+def int32_to_int8(n):
+    mask = (1 << 16) - 1
+    return [(n >> k) & mask for k in range(0, 32, 16)]
+
+
+var_int = utils.encode_ieee(7.5)
+
+sonuc=int32_to_int8(var_int)
+print(sonuc)
+if c.is_open():
+    # read 10 registers at address 0, store result in regs list
+    print(var_int)
+    c.write_single_register(0,5)
+    c.write_multiple_registers(1,sonuc)
+
+    regs = c.read_holding_registers(0,100)
+    # if success display registers
+    if regs:
+        print("reg ad #0 to 9: " , regs)
+
+    bits = c.read_discrete_inputs(0, 16)
+    # if success display registers
+    if bits:
+        print("bit ad #0 to 9: " + str(bits))
