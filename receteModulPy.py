8e67f8a73271e2dcd1f4e9917d68638977b1e095
mbasaran77 <mbasaran77@gmail.com>
2016-09-26 16:58:24 +0300

    Merge branch 'master' of https://github.com/mbasaran77/my_pro001
    Conflicts:
    	vpRecete.ui

--- receteModulPy.py
+++ receteModulPy.py
@@ -12,10 +12,10 @@ class recete_edit(QDialog,vpRecete.Ui_Dialog):
     def __init__(self):
         super(recete_edit, self).__init__()
         self.setupUi(self)
-        self.lineEditBas.setValidator(QDoubleValidator(0.05,1000.00,2))
-        self.lineEditSon.setValidator(QIntValidator(0,65564))
-        #self.lineEditBas.setInputMask("000000.00")
-        #self.lineEditSon.setInputMask("000000.00")
+        self.validator=QDoubleValidator(0.05,1000.00,2)
+        self.validator.setNotation(QDoubleValidator.StandardNotation)
+        self.lineEditBas.setValidator(self.validator)
+        self.lineEditSon.setValidator(self.validator)
         self.btnEkle.clicked.connect(self.ekleItem)
         self.list1.itemClicked.connect(self.list1Index)
         self.btnSil.clicked.connect(self.silItem)
@@ -25,6 +25,15 @@ class recete_edit(QDialog,vpRecete.Ui_Dialog):
         self.btnAc.clicked.connect(self.oku)
 
     def ekleItem(self):
+        text=(self.lineEditBas.text(),self.lineEditSon.text())
+        for text_it in text:
+            isFloat=checkIsFloat(text_it,self.validator)
+            a=isFloat.checkText()
+            if a[0]==False:
+                QMessageBox.warning(self,__appname__,a[1])
+                return
+
+
         self.n_recete.ekle(self.comboBox.currentText(),self.lineEditBas.text(),self.lineEditSon.text())
         self.tableWidget.setRowCount(len(self.n_recete.my_dict))
         self.list1.clear()
@@ -42,7 +51,7 @@ class recete_edit(QDialog,vpRecete.Ui_Dialog):
         index=self.list1Index()
         print("index_list",index)
         if index<0 or index==None:
-            QMessageBox.warning(self,__appname__,"Listede Silinecek Satrı Yok")
+            QMessageBox.warning(self,__appname__,"Listede Silinecek Satır Yok")
         else:
             self.n_recete.kaldir(index)
             self.list1.clear()
@@ -78,6 +87,23 @@ class recete_edit(QDialog,vpRecete.Ui_Dialog):
             for n in range(len(_myDict)):
                 a = str(_myDict[n])
                 self.list1.addItem(a)
+
+class  checkIsFloat():
+    def __init__(self,text,validator):
+        self.text=text
+        self.validator=validator
+
+
+    def checkText(self):
+        text=self.text
+        state = self.validator.validate(self.text, 0)[0]
+        if "," in text:
+            return (False,"virgül yerine nokta kullanılmalı")
+        elif state ==QValidator.Intermediate:
+            return (False,"girilen sayı belirlenen aralıkta değil")
+        else:
+            return (True,"başarılı")
+
 
 
 
